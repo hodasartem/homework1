2a9397e3f532c73e846448a3ff4fd46f198f61e8
KhodasArtyom <hodasartem777@gmail.com>
2021-09-22 19:57:11 +0300

    tasks

---
+++ Task2.py
@@ -0,0 +1,14 @@
+N = int(input('Введите количество секунд: \n'))
+
+hour = N // 3600 
+min = (N - (hour * 3600)) // 60 
+sec = N - (hour * 3600) - (min * 60)
+
+print(hour)
+print(type(hour))
+
+print(min)
+print(type(min))
+
+print(sec)
+print(type(sec))
---
+++ Task3.py
@@ -0,0 +1,6 @@
+my_str = ('''Petya saved the cat from the tree
+Petya rescued a few people from the fire
+A thief was stopped by Petya yesterday evening ''')
+a =my_str. replace('Petya', 'Artyom')
+
+print(a)
