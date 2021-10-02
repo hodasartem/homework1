f8c4e76340d89aec0c31f17539709cce929d5ccb
hodasartem <91064365+hodasartem@users.noreply.github.com>
2021-10-02 09:18:03 +0300

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -4,11 +4,4 @@ hour = N // 3600
 min = (N - (hour * 3600)) // 60 
 sec = N - (hour * 3600) - (min * 60)
 
-print(hour)
-print(type(hour))
-
-print(min)
-print(type(min))
-
-print(sec)
-print(type(sec))
+print(f'{hour} : {min} : {sec}')
